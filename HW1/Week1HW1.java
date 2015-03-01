e7654377403feec93931476d885706043afc2094
zoo88115 <zoo88115@gmail.com>
2015-03-01 15:46:07 +0800

    hw1
    七言絕句

---
+++ HW1/Week1HW1.java
@@ -0,0 +1,12 @@
+class Week1HW1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("杜甫《贈花卿》\n");
+		System.out.println("錦城絲管日紛紛");
+		System.out.println("半入江風半入雲");
+		System.out.println("此曲祗應天上有");
+		System.out.println("人間能得幾回聞");
+	}
+}
+		
